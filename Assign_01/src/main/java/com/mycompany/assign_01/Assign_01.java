63f994e1cf10dff896dd1a2f1fbb7fa84bd2109c
Vnp74 <68412685+vnp74@users.noreply.github.com>
2024-03-23 02:03:57 +1000

    Project Creared
    I created the project and added to the repository.

---
+++ Assign_01/src/main/java/com/mycompany/assign_01/Assign_01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.assign_01;
+
+/**
+ *
+ * @author VRAJ
+ */
+public class Assign_01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
