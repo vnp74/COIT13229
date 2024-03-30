8f7a9aac01c3d25f7751f1d78c1fc72136907847
Vnp74 <68412685+vnp74@users.noreply.github.com>
2024-03-30 22:55:21 +1000

    Add server listening port
    Added server listening port to UDPServer

--- Assign_01/src/main/java/com/mycompany/assign_01/UDPServer.java
+++ Assign_01/src/main/java/com/mycompany/assign_01/UDPServer.java
@@ -4,7 +4,13 @@ import java.io.*;
 import java.net.*;
 
 public class UDPServer {
-    public static void main(String[] args) {
+    private static final int Port = 2225; // Server's listening port (student id - 12191825)
 
+    public static void main(String[] args) {
+        try (DatagramSocket socket = new DatagramSocket(Port)) {
+            System.out.println("UDP Server is running on Port: " + Port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
