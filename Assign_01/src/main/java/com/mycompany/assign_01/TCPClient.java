f14618b547d92c2ef83e45ce74112bfc3713a617
Vnp74 <68412685+vnp74@users.noreply.github.com>
2024-03-30 16:52:11 +1000

    Setup Client/Server
    Setting up the basic communication between the client and the server.

--- Assign_01/src/main/java/com/mycompany/assign_01/TCPClient.java
+++ Assign_01/src/main/java/com/mycompany/assign_01/TCPClient.java
@@ -1,13 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.assign_01;
 
-/**
- *
- * @author VRAJ
- */
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
 public class TCPClient {
-    
+    public static void main(String[] args) {
+        // Server hostname and port configuration.
+        // Ensure these values match your TCPServer configuration.
+        String hostname = "localhost";
+        int port = 1125; // Port number (1125 - 25 is the last two digits of my student id)
+
+        try (Socket socket = new Socket(hostname, port)) {
+            DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+            DataInputStream dis = new DataInputStream(socket.getInputStream());
+            Scanner scanner = new Scanner(System.in);
+        }
+
+        catch (UnknownHostException e) {
+            System.err.println("Server not found: " + e.getMessage());
+        } catch (IOException e) {
+            System.err.println("I/O Error: " + e.getMessage());
+        }
+    }
 }
--- Assign_01/src/main/java/com/mycompany/assign_01/TCPServer.java
+++ Assign_01/src/main/java/com/mycompany/assign_01/TCPServer.java
@@ -1,13 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.assign_01;
 
-/**
- *
- * @author VRAJ
- */
+import java.io.*;
+import java.net.*;
+
 public class TCPServer {
-    
+    private static final int Port = 1125; // Port number (1125 - 25 is the last two digits of my student id)
+
+    public static void main(String[] args) {
+        // server socket looking for clients trying to connect
+        try (ServerSocket serverSocket = new ServerSocket(Port)) {
+            System.out.println("Server is listening on port" + Port);
+        } catch (IOException e) {
+            System.err.println("Server exception: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
