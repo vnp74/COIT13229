a0d4b3a8c93fd39b2f4b4ff6e76d001b30b54d0f
Vnp74 <68412685+vnp74@users.noreply.github.com>
2024-03-30 17:02:53 +1000

    Taking User Input
    Prompting the user to input the details.

--- Assign_01/src/main/java/com/mycompany/assign_01/TCPClient.java
+++ Assign_01/src/main/java/com/mycompany/assign_01/TCPClient.java
@@ -15,6 +15,46 @@ public class TCPClient {
             DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
             DataInputStream dis = new DataInputStream(socket.getInputStream());
             Scanner scanner = new Scanner(System.in);
+
+            while (true) {
+                System.out.println("Please enter the following details for member registration:");
+
+                // Prompting for first name.
+                System.out.print("First Name: ");
+                String firstName = scanner.nextLine();
+
+                if ("exit".equalsIgnoreCase(firstName)) {
+                    break; // Exit loop if user inputs 'exit'.
+                }
+
+                // Prompting for last name.
+                System.out.print("Last Name: ");
+                String lastName = scanner.nextLine();
+
+                // Prompting for address.
+                System.out.print("Address: ");
+                String address = scanner.nextLine();
+
+                // Prompting for phone number.
+                System.out.print("Phone Number: ");
+                String phoneNumber = scanner.nextLine();
+
+                // Sending member details to the server.
+                dos.writeUTF(firstName + "," + lastName + "," + address + "," + phoneNumber);
+
+                // Awaiting and printing server's response.
+                String response = dis.readUTF();
+                System.out.println("Server response: " + response);
+
+                // Asking if the user wants to continue or exit.
+                System.out.println("Do you want to register another member? (yes/no)");
+                String answer = scanner.nextLine();
+
+                if ("no".equalsIgnoreCase(answer)) {
+                    System.out.println("Exiting registration process.");
+                    break;
+                }
+            }
         }
 
         catch (UnknownHostException e) {
