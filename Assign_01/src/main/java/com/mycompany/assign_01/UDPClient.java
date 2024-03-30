7e704e2b35dec3f220feb5e09a5db37653c78491
Vnp74 <68412685+vnp74@users.noreply.github.com>
2024-03-30 22:40:33 +1000

    Basic Setup UDP Client and Server
    Created files UDPClient and UDPServer

---
+++ Assign_01/src/main/java/com/mycompany/assign_01/UDPClient.java
@@ -0,0 +1,10 @@
+package com.mycompany.assign_01;
+
+import java.io.*;
+import java.net.*;
+
+public class UDPClient {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Assign_01/src/main/java/com/mycompany/assign_01/UDPServer.java
@@ -0,0 +1,10 @@
+package com.mycompany.assign_01;
+
+import java.io.*;
+import java.net.*;
+
+public class UDPServer {
+    public static void main(String[] args) {
+
+    }
+}
