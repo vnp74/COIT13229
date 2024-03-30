64eb265beba78d5db3129fb5ce8992e3a0955e5d
Vnp74 <68412685+vnp74@users.noreply.github.com>
2024-03-30 23:06:19 +1000

    set basic communication UDPClient
    Implement basic communication in UDPClient to send 'Ping' to server.

--- Assign_01/src/main/java/com/mycompany/assign_01/UDPClient.java
+++ Assign_01/src/main/java/com/mycompany/assign_01/UDPClient.java
@@ -3,8 +3,27 @@ package com.mycompany.assign_01;
 import java.io.*;
 import java.net.*;
 
-public class UDPClient {
-    public static void main(String[] args) {
+import javax.sound.sampled.Port;
 
+public class UDPClient {
+    private static final int server_port = 2225;
+    private static final String server_ip = "localhost";
+
+    public static void main(String[] args) {
+        try (DatagramSocket socket = new DatagramSocket()) {
+
+            InetAddress serverAddress = InetAddress.getByName(server_ip);
+
+            byte[] sendBuffer = "Ping".getBytes();
+
+            DatagramPacket sendPacket = new DatagramPacket(sendBuffer, serverBuffer.length, serverAddress, server_port);
+            socket.send(sendPacket);
+
+            System.out.println("Ping message sent to server.");
+        }
+
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
